2afdb7fc88946d9ad687725c4244f706fffb0a85
Maximisation <80449754+Maximisation@users.noreply.github.com>
2021-09-25 11:38:26 +0300

    Add files via upload

--- pervoe.cpp
+++ pervoe.cpp
@@ -6,23 +6,26 @@
 
 int main(){
     int t;
-    std::vector<int> v;
+    std::vector<int> v,tt;
     std::cout << "Input the vector (x y z): " <<std::endl;
     for (int i=0; i<3 ;i++){
         std::cin >>t;
         v.push_back(t);
     }
-    std::vector<int> ten;
+    std::vector<std::vector<int> > ten;
     std::cout << "Input the tenzor value (first row then second row and ect.)" <<std::endl;
-    for(int i=0; i<9; i++){
-        std::cin >>t;
-        ten.push_back(t);
+    for(int i=0; i<3; i++) {
+        ten.push_back(tt);
+        for (int j = 0; j < 3; j++) {
+            std::cin >> t;
+            ten[i].push_back(t);
+        }
     }
     int sum;
     for(int j=0; j<3;j++){
         sum=0;
         for(int i=0;i<3;i++){
-            sum +=v[i]*ten[j*3+i];
+            sum +=v[i]*ten[j][i];
         }
         std::cout << sum << "  ";
     }
